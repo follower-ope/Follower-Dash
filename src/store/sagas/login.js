54c8083e7908d89cb69ecdae748ca2b34a6fb588
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-10-24 23:50:04 -0300

    notificacao de erro no login com api

--- src/store/sagas/login.js
+++ src/store/sagas/login.js
@@ -1,4 +1,5 @@
 import { call, put } from 'redux-saga/effects';
+import { store } from 'react-notifications-component';
 import api from '../../services/api';
 
 import { Creators as LoginActions } from '../ducks/login';
@@ -7,23 +8,29 @@ export function* login(data) {
   try {
     const { payload } = data;
 
-    window.localStorage.setItem('token', 123);
-    yield put(LoginActions.loginSuccess(null));
-    window.location = '/home';
+    const response = yield call(api.post, '/auth', {
+      email: payload.data.email,
+      password: payload.data.password,
+    });
 
-    // const response = yield call(api.post, '/auth', {
-    //   email: payload.data.email,
-    //   password: payload.data.password,
-    // });
-
-    // if (response.status === 200) {
-    //   window.localStorage.setItem('token', response.data.token);
-    //   yield put(LoginActions.loginSuccess(null));
-    //   window.location = '/home';
-    // }
+    if (response.status === 200) {
+      window.localStorage.setItem('token', response.data.token);
+      yield put(LoginActions.loginSuccess(null));
+      window.location = '/home';
+    }
   } catch (err) {
-    // const errorMessage = err.message.data.message;
-    // const errorStatus = err.response.status;
+    store.addNotification({
+      message: 'Ocorreu um erro, tente novamente mais tarde',
+      type: 'danger',
+      insert: 'top',
+      container: 'top-right',
+      animationIn: ['animated', 'fadeIn'],
+      animationOut: ['animated', 'fadeOut'],
+      dismiss: {
+        duration: 2000,
+        onScreen: true,
+      },
+    });
     yield put(LoginActions.loginSuccess(null));
   }
 }
