d27aecb971bc09c3b1b77e353485411e1c8c5e6b
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-10-24 23:46:50 -0300

    notificacao de login

--- src/store/sagas/login.js
+++ src/store/sagas/login.js
@@ -7,18 +7,21 @@ export function* login(data) {
   try {
     const { payload } = data;
 
-    const response = yield call(api.post, '/auth', {
-      email: payload.data.email,
-      password: payload.data.password,
-    });
+    window.localStorage.setItem('token', 123);
+    yield put(LoginActions.loginSuccess(null));
+    window.location = '/home';
 
-    if (response.status === 200) {
-      window.localStorage.setItem('token', response.data.token);
-      yield put(LoginActions.loginSuccess(null));
-      window.location = '/home';
-    }
+    // const response = yield call(api.post, '/auth', {
+    //   email: payload.data.email,
+    //   password: payload.data.password,
+    // });
+
+    // if (response.status === 200) {
+    //   window.localStorage.setItem('token', response.data.token);
+    //   yield put(LoginActions.loginSuccess(null));
+    //   window.location = '/home';
+    // }
   } catch (err) {
-    console.log(err);
     // const errorMessage = err.message.data.message;
     // const errorStatus = err.response.status;
     yield put(LoginActions.loginSuccess(null));
