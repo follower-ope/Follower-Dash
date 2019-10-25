01e0034348665cc68518c6d9d16f22421cc016f0
VitorRedfox <vitor_pereira.2013@hotmail.com>
2019-10-24 23:56:22 -0300

    rotas do dash protegida com redirect

--- src/layouts/DashLayout.js
+++ src/layouts/DashLayout.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { Content, Main, Container } from '../styles/components';
 
 import Sidebar from '../components/Sidebar';
 import NavMenu from '../components/NavMenu';
 
-const DashLayout = ({ children }) => (
-  <>
-    <Content>
-      <Sidebar />
-      <Main>
-        <NavMenu />
-        <Container>{children}</Container>
-      </Main>
-    </Content>
-  </>
-);
+const DashLayout = ({ children }) => {
+  useEffect(() => {
+    if (!window.localStorage.getItem('token')) {
+      window.location = '/login';
+    }
+  }, []);
+
+  return (
+    <>
+      <Content>
+        <Sidebar />
+        <Main>
+          <NavMenu />
+          <Container>{children}</Container>
+        </Main>
+      </Content>
+    </>
+  );
+};
 
 export default DashLayout;
